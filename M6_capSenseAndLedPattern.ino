d64b8bfdbb166b1170ff5594a04138e1f65116e4
Phickies <51713512+Phickies@users.noreply.github.com>
2024-01-19 17:30:11 +0100

    Update M6_capSenseAndLedPattern.ino

--- M6_capSenseAndLedPattern.ino
+++ M6_capSenseAndLedPattern.ino
@@ -1,9 +1,12 @@
 #include <CapacitiveSensor.h>
 #include <Adafruit_NeoPixel.h>
+#include <Wire.h>
 
-//Pins for controler
+// Pin to use to for controller
 #define CON_R_PIN_OUT A0
-#define CON_R_PIN_IN A1
+#define CON_L_PIN_OUT A1
+#define CON_R_PIN_IN A2
+#define CON_L_PIN_IN A3
 
 // Pin to use to send signals to WS2812B
 #define LED_PIN1 9
@@ -15,6 +18,14 @@
 // Number of WS2812B LEDs attached to the Arduino
 #define LED_COUNT 47
 #define LED_COUNTbob 82
+
+#define CAP_SEN_MIN_1 5000
+#define CAP_SEN_MIN_2 3000
+#define CAP_SEN_MIN_3 8000
+#define CAP_SEN_MIN_4 4000
+
+#define PATTERN_SPEED 2000
+#define GAME_LENGTH 6000
 
 // Setting up the NeoPixel library
 Adafruit_NeoPixel strip1(LED_COUNT, LED_PIN1, NEO_GRB + NEO_KHZ800);
@@ -28,19 +39,13 @@ CapacitiveSensor cs2 = CapacitiveSensor(4, 3);
 CapacitiveSensor cs3 = CapacitiveSensor(6, 5);
 CapacitiveSensor cs4 = CapacitiveSensor(8, 7);
 
-int capSensor1Min;
-int capSensor2Min;
-int capSensor3Min;
-int capSensor4Min;
 int patternNum;  //Which tile to step on
 int prevPatternNum;
-int patternSpeed;  //The time spent on each tile
-unsigned long previousMillis;
-unsigned long gameStart;  // the time a game starts
-int missed;               //How may tiles missed per game
-int previousMissed;
-int gameLength;  //How long each game lasts
 
+unsigned long previousMillis;
+unsigned long startTime;
+int missed;  //How may tiles missed per game
+int previousMissed;
 
 bool tile1;  //back right
 bool tile2;  // back left
@@ -48,28 +53,28 @@ bool tile3;  //front right
 bool tile4;  //front left
 bool isPlaying;
 bool isShakedR;
+bool isShakedL;
+
+const int MPU_ADDR = 0x68;
+int16_t accelerometer_x, accelerometer_y, accelerometer_z;  // variables for accelerometer raw data
+int16_t gyro_x, gyro_y, gyro_z;                             // variables for gyro raw datas
+
+char tmp_str[7];  // temporary variable used in convert function
 
 void setup() {
 
   Serial.begin(9600);
 
-  pinMode(CON_R_PIN_OUT, OUTPUT);
-  pinMode(CON_R_PIN_IN, INPUT);
-  digitalWrite(CON_R_PIN_IN, LOW);
-
-
-  capSensor1Min = 5000;
-  capSensor2Min = 3000;
-  capSensor3Min = 8000;
-  capSensor4Min = 4000;
-
-  previousMillis = 0;
-  patternSpeed = 2000;
   missed = 0;
-  previousMissed = 0;
+  previousMillis = 0;
   isPlaying = false;
-  gameLength = 30000;
 
+  pinMode(CON_R_PIN_OUT, OUTPUT);
+  pinMode(CON_L_PIN_OUT, OUTPUT);
+  pinMode(CON_R_PIN_IN, INPUT);
+  pinMode(CON_L_PIN_IN, INPUT);
+  digitalWrite(CON_R_PIN_IN, LOW);
+  digitalWrite(CON_L_PIN_IN, LOW);
 
   strip1.begin();              // Initialize NeoPixel object
   strip1.setBrightness(10);    // Set BRIGHTNESS to about 4% (max = 255)
@@ -89,31 +94,34 @@ void setup() {
 
   // The first NeoPixel in a strand is #0, second is 1, all the way up
   // to the count of pixels minus one.
+
+  Wire.begin();
+  Wire.beginTransmission(MPU_ADDR);  // Begins a transmission to the I2C slave (GY-521 board)
+  Wire.write(0x6B);                  // PWR_MGMT_1 register
+  Wire.write(0);                     // set to zero (wakes up the MPU-6050)
+  Wire.endTransmission(true);
 }
 
 void loop() {
   unsigned long currentMillis = millis();
 
-  if (currentMillis - previousMillis >= patternSpeed && isPlaying == true) {
-    checkTiles();  //Check if the tile has been pressed at any moment during the waiting
+  if (currentMillis - previousMillis >= PATTERN_SPEED
+      && isPlaying == true) {
+    checkTiles();
     updatePattern();
     previousMillis = currentMillis;
   }
 
   if (isPlaying == false && tile3 == true && tile4 == true) {
-    isPlaying = true;
-    gameStart = millis();
-    Serial.println("Started");
+    gameStart();
   }
 
-  if (isPlaying == true && currentMillis - gameStart >= gameLength) {
-    gameStart = 0;
-    isPlaying = false;
-    missed = 0;
-    previousMissed = 0;
-    Serial.println("Game End");
-    resetTiles();
-    standByBob();
+  if (isPlaying == false){
+    Serial.print("Standby");
+  }
+
+  if (isPlaying == true && currentMillis - startTime >= GAME_LENGTH) {
+    gameOver();
   }
 
   readSensors();
@@ -125,9 +133,9 @@ void updatePattern() {
   resetTiles();
 
   // Randomly select a new pattern, ensuring it's different from the previous one
-  patternNum = random(1, 6);
+  patternNum = random(1, 7);
   while (patternNum == prevPatternNum) {
-    patternNum = random(1, 6);
+    patternNum = random(1, 7);
   }
   prevPatternNum = patternNum;
 
@@ -147,6 +155,9 @@ void updatePattern() {
   }
   if (patternNum == 5) digitalWrite(CON_R_PIN_OUT, HIGH);
   else digitalWrite(CON_R_PIN_OUT, LOW);
+
+  if (patternNum == 6) digitalWrite(CON_L_PIN_OUT, HIGH);
+  else digitalWrite(CON_L_PIN_OUT, LOW);
 }
 
 void checkTiles() {
@@ -156,6 +167,7 @@ void checkTiles() {
   if (patternNum == 3 && !tile3) missed++;
   if (patternNum == 4 && !tile4) missed++;
   if (patternNum == 5 && !isShakedR) missed++;
+  if (patternNum == 6 && !isShakedL) missed++;
 
   if (missed > previousMissed) {
     for (int i = 0; i < LED_COUNTbob; i++) {
@@ -172,16 +184,18 @@ void readSensors() {
   long total3 = cs3.capacitiveSensor(30);
   long total4 = cs4.capacitiveSensor(30);
 
-  tile1 = (total1 > capSensor1Min);
-  tile2 = (total2 > capSensor2Min);
-  tile3 = (total3 > capSensor3Min);
-  tile4 = (total4 > capSensor4Min);
+  tile1 = (total1 > CAP_SEN_MIN_1);
+  tile2 = (total2 > CAP_SEN_MIN_2);
+  tile3 = (total3 > CAP_SEN_MIN_3);
+  tile4 = (total4 > CAP_SEN_MIN_4);
   isShakedR = (digitalRead(CON_R_PIN_IN) == LOW);
+  isShakedL = acceleratorRead();
 }
 
 void resetTiles() {
   tile1 = tile2 = tile3 = tile4 = false;
   isShakedR = false;
+  isShakedL = false;
 }
 
 void updateLEDs() {
@@ -196,20 +210,78 @@ void updateLEDs() {
 void updateController() {
   if (isShakedR) {
     digitalWrite(CON_R_PIN_OUT, LOW);
-    //Serial.println("isShaked");
+    Serial.println("isShaked RIGHT");
+  }
+  if (isShakedL) {
+    digitalWrite(CON_L_PIN_OUT, LOW);
+    Serial.println("isShaked LEFT");
   }
 }
 
-void standByBob() {
+char* convert_int16_to_str(int16_t i) {  // converts int16 to string. Moreover, resulting strings will have the same length in the debug monitor.
+  sprintf(tmp_str, "%6d", i);
+  return tmp_str;
+}
+
+bool acceleratorRead() {
+  Wire.beginTransmission(MPU_ADDR);
+  Wire.write(0x3B);                         // starting with register 0x3B (ACCEL_XOUT_H) [MPU-6000 and MPU-6050 Register Map and Descriptions Revision 4.2, p.40]
+  Wire.endTransmission(false);              // the parameter indicates that the Arduino will send a restart. As a result, the connection is kept active.
+  Wire.requestFrom(MPU_ADDR, 7 * 2, true);  // request a total of 7*2=14 registers
+
+  // "Wire.read()<<8 | Wire.read();" means two registers are read and stored in the same variable
+  accelerometer_x = Wire.read() << 8 | Wire.read();  // reading registers: 0x3B (ACCEL_XOUT_H) and 0x3C (ACCEL_XOUT_L)
+  accelerometer_y = Wire.read() << 8 | Wire.read();  // reading registers: 0x3D (ACCEL_YOUT_H) and 0x3E (ACCEL_YOUT_L)
+  accelerometer_z = Wire.read() << 8 | Wire.read();  // reading registers: 0x3F (ACCEL_ZOUT_H) and 0x40 (ACCEL_ZOUT_L)
+  gyro_x = Wire.read() << 8 | Wire.read();           // reading registers: 0x43 (GYRO_XOUT_H) and 0x44 (GYRO_XOUT_L)
+  gyro_y = Wire.read() << 8 | Wire.read();           // reading registers: 0x45 (GYRO_YOUT_H) and 0x46 (GYRO_YOUT_L)
+  gyro_z = Wire.read() << 8 | Wire.read();           // reading registers: 0x47 (GYRO_ZOUT_H) and 0x48 (GYRO_ZOUT_L)
+
+  // print out data
+  Serial.print("aX = ");
+  Serial.print(convert_int16_to_str(accelerometer_x));
+  Serial.print(" | aY = ");
+  Serial.print(convert_int16_to_str(accelerometer_y));
+  Serial.print(" | aZ = ");
+  Serial.print(convert_int16_to_str(accelerometer_z));
+  // the following equation was taken from the documentation [MPU-6000/MPU-6050 Register Map and Description, p.30]
+  Serial.print(" | tmp = ");
+  Serial.print(" | gX = ");
+  Serial.print(convert_int16_to_str(gyro_x));
+  Serial.print(" | gY = ");
+  Serial.print(convert_int16_to_str(gyro_y));
+  Serial.print(" | gZ = ");
+  Serial.print(convert_int16_to_str(gyro_z));
+  Serial.println();
+
+  // delay
+  delay(10);
+
+  return false;
+}
+
+void gameStart() {
+  isPlaying = true;
+  startTime = millis();
+  Serial.println("Started");
+}
+
+void gameOver() {
+  isPlaying = false;
+  missed = 0;
+  previousMissed = 0;
+  Serial.println("Game End");
+  resetTiles();
+
   for (int i = 0; i < LED_COUNTbob - 24; i++) {
     stripbob.setPixelColor(i, 0, 255, 0);  // Green ambient light
   }
 
-  int maxScore = int(gameLength / patternSpeed);
-  int ledScore = int(missed/maxScore * 24);
+  int maxScore = int(GAME_LENGTH / PATTERN_SPEED);
+  int ledScore = int(missed / maxScore * 24);
   Serial.println(ledScore);
 
-  for (int i = ledScore; i > 0 - 24; i--) {
+  for (int i = ledScore; i > 0; i--) {
     stripbob.setPixelColor(i, 0, 255, 0);  // Light up the score bar based on score
   }
 }
